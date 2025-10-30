5b379d8906423f6a36869cfb4cb55c6963cbb788
Dipen-sigzen <dipen.f@sigzen.com>
2025-10-30 18:04:15 +0530

    test

--- setup.py
+++ setup.py
@@ -2,3 +2,14 @@ from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
+
+
+setup(
+    name='dsc_erpnext',
+    version='0.0.1',
+    description='Custom ERPNext app',
+    author='Your Name',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
